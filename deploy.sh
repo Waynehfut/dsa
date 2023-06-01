8b5bee4c88a0bd6c5f0482a41087fdb01977d0cb
Hao Wang <waynehfut@outlook.com>
2023-06-01 18:35:33 +0800

    add based architecture

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
 set -e
-yarn build
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
 cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+ echo 'dsa.waynehfut.com' > CNAME
 
 git init
 git add -A
