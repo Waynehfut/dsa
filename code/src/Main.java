f64c294d16a4c6e38b81ee009c15254a42257f39
Hao Wang <waynehfut@outlook.com>
2023-06-19 11:48:35 +0800

    update chap2

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println("The output is " + a[1]);
-        a[1] = 5;
-        System.out.println("The output is " + a[1]);
+        int[] arr = new int[10];
+        int[] expArr = new int[arr.length * 2];
+        for (int i = 0; i < arr.length; i++) {
+            expArr[i] = arr[i];
+        }
+        arr = expArr;
     }
 }
